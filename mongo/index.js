90e042350c69e283b5fd5eb290d4b66cff86abc6
Duarte M <64815777+ozzypt@users.noreply.github.com>
2024-06-07 13:50:52 +0100

    changes in mongo connection

--- mongo/index.js
+++ mongo/index.js
@@ -1,11 +1,44 @@
+// // connect to atlas and return the connection "connectToDatabase"
+
+// import { MongoClient } from "mongodb";
+
+// // export async function connectToDatabase() {
+// //   const client = await MongoClient.connect(process.env.DB_URI, {});
+// //   return client;
+// // }
+
+// let cached = global.mongo;
+
+// if (!cached) {
+//   cached = global.mongo = { conn: null, promise: null };
+// }
+
+// export async function connectToDatabase() {
+//   if (cached.conn) {
+//     return cached.conn;
+//   }
+
+//   if (!cached.promise) {
+//     const opts = {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     };
+//     cached.promise = MongoClient.connect(process.env.DB_URI, opts).then(
+//       (client) => {
+//         cached.conn = client;
+//         return client;
+//       }
+//     );
+//   }
+
+//   await cached.promise;
+//   return cached.conn;
+// }
+
+//NEW VERSION
 // connect to atlas and return the connection "connectToDatabase"
 
 import { MongoClient } from "mongodb";
-
-// export async function connectToDatabase() {
-//   const client = await MongoClient.connect(process.env.DB_URI, {});
-//   return client;
-// }
 
 let cached = global.mongo;
 
@@ -19,10 +52,7 @@ export async function connectToDatabase() {
   }
 
   if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
+    const opts = {};
     cached.promise = MongoClient.connect(process.env.DB_URI, opts).then(
       (client) => {
         cached.conn = client;
